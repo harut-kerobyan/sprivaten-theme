322f443ee4e9a6548f7c9d82e1a7c3a3bb418e61
harut-kerobyan <kerobyanharut@gmail.com>
2023-07-20 21:43:03 +0400

    Appointment admin page

--- functions.php
+++ functions.php
@@ -2,3 +2,4 @@
 include_once( get_template_directory() . '/inc/helper-functions.php' );
 include_once( get_template_directory() . '/inc/theme-settings.php' );
 include_once( get_template_directory() . '/inc/ajax-callback.php' );
+include_once( get_template_directory() . '/inc/admin-pages.php' );
---
+++ inc/admin-pages.php
@@ -0,0 +1,44 @@
+<?php
+function appointments_admin_page_callback() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'appointment';
+    $results = $wpdb->get_results("SELECT * FROM {$table_name} ORDER BY `created_at` DESC");
+
+    ?>
+    <pre><?php //print_r($results); ?></pre>
+    <div class="wrap">
+        <h1>All Appointments</h1>
+
+        <?php if (!empty($results)) : ?>
+        <table class="wp-list-table widefat fixed striped table-view-list">
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Department</th>
+                <th>Time</th>
+                <th>Message</th>
+                <th>Submission date</th>
+            </tr>
+            <?php foreach ($results as $row) : ?>
+            <tr>
+                <td><?php echo $row->name; ?></td>
+                <td><?php echo $row->email; ?></td>
+                <td><?php echo $row->department; ?></td>
+                <td><?php echo $row->time; ?></td>
+                <td><?php echo $row->message; ?></td>
+                <td><?php echo $row->created_at; ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <?php else : ?>
+            <p>No data yet</p>
+        <?php endif; ?>
+    </div>
+    <?php
+}
+
+function add_custom_admin_page() {
+    add_menu_page('All Appointments', 'Appointments', 'manage_options', 'appointments-page', 'appointments_admin_page_callback', 'dashicons-calendar-alt', 30);
+}
+
+add_action('admin_menu', 'add_custom_admin_page');
