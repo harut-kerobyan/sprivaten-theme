9c3dd4d61516afb1d0db1a29e432eb88555d1949
harut-kerobyan <kerobyanharut@gmail.com>
2023-07-20 22:06:57 +0400

    Hide section functionality

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -7,33 +7,34 @@ get_header();
 
 the_post();
 
-?>
-    <div id="page-<?php the_ID(); ?>" <?php post_class( 'home-content' ); ?>>
-        <?php if( have_rows('components') ) : ?>
-            <?php while ( have_rows('components') ) : the_row(); ?>
-                <?php if( get_row_layout() == 'hero_section' ) : ?>
-                    <?php get_template_part('sections/section', 'hero'); ?>
-                <?php endif; ?>
-                <?php if( get_row_layout() == 'why_choose_us_section' ) : ?>
-                    <?php get_template_part('sections/section', 'why_choose_us'); ?>
-                <?php endif; ?>
-                <?php if( get_row_layout() == 'video_section' ) : ?>
-                    <?php get_template_part('sections/section', 'video'); ?>
-                <?php endif; ?>
-                <?php if( get_row_layout() == 'testimonials_section' ) : ?>
-                    <?php get_template_part('sections/section', 'testimonials'); ?>
-                <?php endif; ?>
-                <?php if( get_row_layout() == 'team_section' ) : ?>
-                    <?php get_template_part('sections/section', 'team'); ?>
-                <?php endif; ?>
-                <?php if( get_row_layout() == 'contact_us_section' ) : ?>
-                    <?php get_template_part('sections/section', 'contact_us'); ?>
-                <?php endif; ?>
-                <?php if( get_row_layout() == 'cta_section' ) : ?>
-                    <?php get_template_part('sections/section', 'cta'); ?>
-                <?php endif; ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
-<?php
+if (have_rows('components')) {
+    while (have_rows('components')) {
+        the_row();
+
+        if (!get_sub_field('hide_section')) {
+            if (get_row_layout() == 'hero_section') {
+                get_template_part('sections/section', 'hero');
+            }
+            if (get_row_layout() == 'why_choose_us_section') {
+                get_template_part('sections/section', 'why_choose_us');
+            }
+            if (get_row_layout() == 'video_section') {
+                get_template_part('sections/section', 'video');
+            }
+            if (get_row_layout() == 'testimonials_section') {
+                get_template_part('sections/section', 'testimonials');
+            }
+            if (get_row_layout() == 'team_section') {
+                get_template_part('sections/section', 'team');
+            }
+            if (get_row_layout() == 'contact_us_section') {
+                get_template_part('sections/section', 'contact_us');
+            }
+            if (get_row_layout() == 'cta_section') {
+                get_template_part('sections/section', 'cta');
+            }
+        }
+    }
+}
+
 get_footer();
